4eee255c7c2d55a20e50c82547c435a4074ba28a
Fedor Strizhnev <fedor_qd@mail.ru>
2016-05-05 21:19:13 +0300

    Move rho-Pollard algorythm to separate code

--- main.cpp
+++ main.cpp
@@ -1,64 +1,6 @@
 #include <iostream>
 #include <assert.h>
-
-template <typename T>
-T gcd(T a, T b){
-    static_assert(std::is_integral<T>::value, "Only integer types allowed");
-
-    if(a<0) a=-a;
-    if(b<0) b=-b;
-
-    if(a%b==0) return b;
-    if(b%a==0) return a;
-    if(b==0) return (a>0)? a : -a;
-
-    T c = 1;
-    while((a%2 == 0)&&(b%2 == 0)){
-        a>>=1;
-        b>>=1;
-        c<<=1;
-    }
-
-    while(a!=b){
-        if(a%2 == 0)
-            a>>=1;
-        if(a>b)
-            a-=b;
-        else
-            b-=a;
-        if(b%2 == 0)
-            b>>=1;
-    }
-    return c*a;
-}
-
-template <typename T>
-T fx(T x, T a, T c, T modN){
-    auto tmp = (a*x*x + c)%modN;
-//    assert(tmp==0); // for tested simple numbers always tmp==0
-    return tmp;
-}
-
-template <typename T>
-T PoPollard(T src){
-    static_assert(std::is_integral<T>::value, "Only integer types allowed");
-    T x1=2, x2=2, a=1, c=1, d=1;
-    while(d==1){
-        x1=fx(x1, a, c, src);
-        x2=fx(fx(x2, a, c, src), a, c, src);
-
-        if(x2==0) return 1; // looks like when src is simple number f(f(x))==0
-
-        if(x1==x2){
-            x1=2, x2=2;
-            a++;
-            continue;
-        }
-
-        d=gcd(abs(x1)-abs(x2), src);
-    }
-    return src/d;
-}
+#include "PO-pollard.hpp"
 
 int main()
 {

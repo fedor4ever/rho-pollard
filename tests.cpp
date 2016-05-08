51069f933f5cc2dcada3bf7e67656af09fd67d9c
Fedor Strizhnev <fedor_qd@mail.ru>
2016-05-08 11:48:23 +0300

    Edit inaccuracy

--- tests.cpp
+++ tests.cpp
@@ -10,12 +10,12 @@ int main()
     std::cout << "Hello gcd:\t" << gcd(256,1) << "\n";
 
     std::cout << "Starting tests for rho-Pollard algorithm\n";
-    std::cout << "\nTesting Po-Pollard with 4253:\t" << PoPollard(4253) << "\n"; //4253
-    std::cout << "\nTesting Po-Pollard with 4256:\t" << PoPollard(4256) << "\n";
-    std::cout << "\nTesting Po-Pollard with -79:\t" << PoPollard(-79) << "\n";
-    std::cout << "\nTesting Po-Pollard with 0:\t" << PoPollard(0) << "\n";
-    std::cout << "\nTesting Po-Pollard with 1:\t" << PoPollard(1) << "\n";
-    std::cout << "\nTesting Po-Pollard with -1:\t" << PoPollard(-1) << "\n";
+    std::cout << "\nTesting rho-Pollard with 4253:\t" << PoPollard(4253) << "\n"; //4253
+    std::cout << "\nTesting rho-Pollard with 4256:\t" << PoPollard(4256) << "\n";
+    std::cout << "\nTesting rho-Pollard with -79:\t" << PoPollard(-79) << "\n";
+    std::cout << "\nTesting rho-Pollard with 0:\t" << PoPollard(0) << "\n";
+    std::cout << "\nTesting rho-Pollard with 1:\t" << PoPollard(1) << "\n";
+    std::cout << "\nTesting rho-Pollard with -1:\t" << PoPollard(-1) << "\n";
 
     return 0;
 }
